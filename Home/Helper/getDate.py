a0c260b5e29973071049d9c06fc9ecacc0d8f83b
Kanha Tomar <kanharajput91@gmail.com>
2023-01-02 22:17:30 +0530

    data sent to templates using python, dynamic content

---
+++ Home/Helper/getDate.py
@@ -0,0 +1,3 @@
+# this will return the date element of the each dictionary 
+def getDate(post):
+    return post['date']
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .Helper.BlogDetailsData import all_blogs     # getting the blogs data
+from .Helper.getDate import getDate             # getting the function which is used to sort the post
 
 # Create your views here.
 
 def home (request):
-    return render(request,'Home/home.html')
+    # sort the list according to the date of dictionaries in accending order
+    # https://github.com/Kanharajput/SortListHavingDictionary/blob/main/sortListHavingDict.txt
+    # check the example of above link to understand happing behind the scene
+    sorted_blogs = sorted(all_blogs,key=getDate)      
+    latest_blogs = sorted_blogs[-3:]               # it will return the recent 3 blogs
+    return render(request,'Home/home.html',{'latest_blogs':latest_blogs})  
