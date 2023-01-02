9745163ab3c109e0ad360c4dc7ed1f1d604ed714
Kanha Tomar <kanharajput91@gmail.com>
2023-01-02 15:31:47 +0530

    pages linked

--- AllBlogs/urls.py
+++ AllBlogs/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.showAllBlogs),                       # http://127.0.0.1:8000/allblogs/
-    path('<str:blog_name>',views.blogDetails)            # http://127.0.0.1:8000/allblogs/blogname
+    path('',views.showAllBlogs,name="allBlogs_page"),                       # http://127.0.0.1:8000/allblogs/
+    path('<str:blog_name>',views.blogDetails,name="blogDetails_page")            # http://127.0.0.1:8000/allblogs/blogname
 ]
--- Home/urls.py
+++ Home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),           # http://127.0.0.1:8000/
+    path('',views.home, name="home_page"),           # http://127.0.0.1:8000/
 ]
