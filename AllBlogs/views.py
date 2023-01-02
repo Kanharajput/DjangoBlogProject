dd1697fbd5dec14b08fd4a5cfcf58f794251c54e
Kanha Tomar <kanharajput91@gmail.com>
2023-01-02 22:24:55 +0530

    dynamically data set to all_blogs app

--- AllBlogs/views.py
+++ AllBlogs/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .Helper.BlogDetailsData import all_blogs
 # Create your views here.
 
 # render the all_blogs html page
 def showAllBlogs(request):
-    return render(request,'AllBlogs/all_blogs.html')
+    return render(request,'AllBlogs/all_blogs.html',{'all_blogs':all_blogs})
 
 def blogDetails(request,blog_name):
     return render(request,'AllBlogs/blog_detail.html')
