47820206317bab26f789a9c5166aac9205feb483
Kanha Tomar <kanharajput91@gmail.com>
2023-01-06 18:46:14 +0530

    data passed to all_blogs and details page

--- AllBlogs/views.py
+++ AllBlogs/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
 
 # removed the data local data now the data is fetched from database
 # render the all_blogs html page
 def showAllBlogs(request):
-    return render(request,'AllBlogs/all_blogs.html')
+    posts = Post.objects.all()
+    return render(request,'AllBlogs/all_blogs.html',{"all_blogs":posts})
 
 def blogDetails(request,blog_name):
-            return render(request,'AllBlogs/blog_detail.html')
+    # post = Post.objects.get(slug=blog_name) if post not found so return 404 template
+    post = get_object_or_404(Post, slug=blog_name)
+    return render(request,'AllBlogs/blog_detail.html',{"clicked_blog":post})
