8773b591fdcf89a66ba65b1ccef7af25dadcc71a
Kanha Tomar <kanharajput91@gmail.com>
2023-01-11 12:45:01 +0530

    Save comment to the database

--- AllBlogs/views.py
+++ AllBlogs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,View
 from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,17 +15,38 @@ class ShowAllBlogs(ListView):
     template_name = "AllBlogs/all_blogs.html"
     
 
-class BlogDetails(DetailView):
-    template_name = "AllBlogs/blog_detail.html"
-    model = Post
-    context_object_name = "clicked_blog"
+class BlogDetails(View):
+    def get(self,request,slug):                         # it also accept slug as arguement
+        clicked_blog = Post.objects.get(slug=slug)       # remember get return a single object but filter return a queryset                   
+        context = {
+                    "clicked_blog":clicked_blog,
+                        "tags":clicked_blog.tags.all(),             # tags can have multiple entries
+                            "comment_form":CommentForm()              # initialise the form 
+                             }
+        return render(request,"AllBlogs/blog_detail.html",context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # object is the Blog which details are showing right now 
-        # tags is many to many field that'swhy we are accessing all tags
-        tags = self.object.tags.all()               
-        context["post_tags"] = tags
-        context["comment_form"] = CommentForm()
-        return context
+
+    # as this post method is coming from the same url that is blogDetails_page 
+    # and this url also need a slug means by default this request contains the slug
+    def post(self,request,slug):
+        clicked_blog = Post.objects.get(slug=slug)
+        # directly fetch the form from the POST request
+        comment_form = CommentForm(request.POST) 
+        # check it's valid , if valid save the form data
+        # directly save data as it's a model form
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)        # not save to database initiate the comment object with user entered data
+            comment.post = clicked_blog                   # manually link comment with the blog
+            comment.save()                                # save to the database
+            return HttpResponseRedirect(reverse("blogDetails_page", args=[slug])) # return to this same url again
+
+        # FORM NOT VALID, then return to the same page with data to the fields which is already entered by the user
+        context = {
+                    "clicked_blog":clicked_blog,
+                        "tags":clicked_blog.tags.all(),             
+                            "comment_form":comment_form                # return the same comment form
+                             }
+        return render(request,"AllBlogs/blog_detail.html",context)
+
+
     
