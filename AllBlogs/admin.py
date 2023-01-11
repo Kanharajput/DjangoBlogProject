86241e6b320c13139459fa0b109d340b7c733f3a
Kanha Tomar <kanharajput91@gmail.com>
2023-01-11 14:35:42 +0530

    register Comment model to admin panel

--- AllBlogs/admin.py
+++ AllBlogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post,Author,Tag
+from .models import Post,Author,Tag,Comment
 
 # register this class as well with Post model then you will see the difference
 class PostAdmin(admin.ModelAdmin):
@@ -7,8 +7,11 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title","date","author",)             # show entries with this three columns value
     list_filter = ("date","author",)                         # let us filter with this two columns
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display  = ("user_name","post",)      # show username and post to represent comments in admin panel
 
 # Register your models here.
 admin.site.register(Post,PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)
+admin.site.register(Comment,CommentAdmin)
